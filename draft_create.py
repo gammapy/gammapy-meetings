f1db153d3a384d00dcfddabb9d627545ca375cca
hstapel <stapel@apc.in2p3.fr>
2023-10-25 15:23:46 +0200

    draft create without token

---
+++ draft_create.py
@@ -0,0 +1,17 @@
+import sys
+
+from github import Github
+
+
+def get_github_repo():
+    print("beginning")
+    #gh = Github(token)
+    gh = Github()
+    repo_gammapy = gh.get_repo("gammapy/gammapy")
+    print(repo_gammapy.get_pulls().get_page(1))
+    print("end")
+
+
+if __name__ == "__main__":
+    #get_github_repo(sys.argv[1])
+    get_github_repo()
