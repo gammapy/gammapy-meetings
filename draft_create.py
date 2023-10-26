b56aea2d88d52986a4281a3a3c5dd37ae6bdf5c8
hstapel <stapel@apc.in2p3.fr>
2023-10-26 11:12:28 +0200

    adding secret token as env -draft create test

--- draft_create.py
+++ draft_create.py
@@ -1,17 +1,22 @@
 import sys
 
 from github import Github
+import os
 
 
 def get_github_repo():
+    try:
+        token = os.environ["SECRET_GITHUB_TOKEN"]
+    except KeyError:
+        token = "Token not available!"
     print("beginning")
-    #gh = Github(token)
-    gh = Github()
+    gh = Github(token)
+    # gh = Github()
     repo_gammapy = gh.get_repo("gammapy/gammapy")
     print(repo_gammapy.get_pulls().get_page(1))
     print("end")
 
 
 if __name__ == "__main__":
-    #get_github_repo(sys.argv[1])
-    get_github_repo()
+    get_github_repo(sys.argv[1])
+    # get_github_repo()
