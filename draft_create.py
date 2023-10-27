9d9d88574b5698ec27a8daf0e25d2209aa0df339
hstapel <stapel@apc.in2p3.fr>
2023-10-27 11:45:07 +0200

    test workflow without token - v min

--- draft_create.py
+++ draft_create.py
@@ -2,19 +2,41 @@ import sys
 
 from github import Github
 import os
+import os
+from datetime import datetime, timedelta
+import pytz
 
-
+#token = os.environ["SECRET_GITHUB_TOKEN"]
+#def get_github_repo(token):
 def get_github_repo():
-    try:
-        token = os.environ["SECRET_GITHUB_TOKEN"]
-    except KeyError:
-        token = "Token not available!"
+    # try:
+    #     token = os.environ["SECRET_GITHUB_TOKEN"]
+    # except KeyError:
+    #     token = "Token not available!"
+    #     print(token)
     print("beginning")
-    gh = Github(token)
-    # gh = Github()
+    #gh = Github(token)
+    gh = Github()
     repo_gammapy = gh.get_repo("gammapy/gammapy")
     print(repo_gammapy.get_pulls().get_page(1))
     print("end")
+
+    current_directory = os.getcwd()
+
+    today = str(datetime.today().strftime('%Y-%m-%d'))
+    time_now = str(datetime.today().strftime('%d/%m/%Y, %H:%M:%S'))
+    today_long = str(datetime.today().strftime('%A, %B %d, %Y'))
+
+    today_report = os.path.join(current_directory, "dev-meetings", str(datetime.today().date().year), today)
+    isExist = os.path.exists(today_report)
+
+    title = f'# Gammapy Developer Meeting \n'
+    report_heading = f' * {today_long}, at 2 pm (CET) \n ' \
+                     '* Gammapy Developer Meeting on Zoom (direct link on Slack) \n' \
+                     '# Agenda\n'
+
+    report_footer = f'\n report created at {time_now}'
+    print(f'isExist {isExist} title {title} report heading {report_heading} report footer {report_footer}')
 
 
 if __name__ == "__main__":
