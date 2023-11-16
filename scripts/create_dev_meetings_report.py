dfa9946a299d0967c3138dc89c03c6082b9f7463
Régis Terrier <rterrier@apc.in2p3.fr>
2023-11-16 18:14:38 +0100

    Update script and change cron
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- scripts/create_dev_meetings_report.py
+++ scripts/create_dev_meetings_report.py
@@ -29,30 +29,36 @@ report_footer = f'\n report created at {time_now}'
 # ***************** connecting to the repository of gammapy with token ***************
 
 token = os.environ["SUPER_SECRET"]
-gh = Github(token)
+gh = Github(token, per_page=200)
 repo_gammapy = gh.get_repo("gammapy/gammapy")
 
 # ***************** getting pulls**********************
-gammapy_pulls_all_open = repo_gammapy.get_pulls('open')
-gammapy_pulls_all_closed = repo_gammapy.get_pulls('closed')
-gammapy_issues = repo_gammapy.get_issues()
-pulls_page1 = gammapy_pulls_all_open.get_page(0)
-
+gammapy_pulls_all_open = repo_gammapy.get_pulls('open').get_page(0)
+gammapy_pulls_all_closed = repo_gammapy.get_pulls('closed').get_page(0)
+gammapy_issues = repo_gammapy.get_issues(state='open').get_page(0)
+#pulls_page1 = gammapy_pulls_all_open.get_page(0)
 # *************** one week long **********************
 days = 8
 d = datetime.today() - timedelta(days=days)
 
 
-def list_items(text, items):
+def list_items(text, items, closed=False):
     for i in items:
-        if i.created_at.replace(tzinfo=pytz.utc) > d.replace(tzinfo=pytz.utc):
+        time = i.closed_at if closed else i.created_at
+        if time.replace(tzinfo=pytz.utc) > d.replace(tzinfo=pytz.utc):
             text = text + f'* [#{i.number}]({i.html_url}) {i.title} - {i.user.name}\n'
     return text
 
+def list_created_issues(text, issues):
+    for i in issues:
+        if i.pull_request is None:
+            if i.created_at.replace(tzinfo=pytz.utc) > d.replace(tzinfo=pytz.utc):
+                text = text + f'* [#{i.number}]({i.html_url}) {i.title} - {i.user.name}\n'
+    return text
 
 pulls_text = list_items(f'\n### PRs opened last week (less than {days} days ago): \n', gammapy_pulls_all_open)
-merged_text = list_items(f'\n### PRs merged last week (less than {days} days ago): \n', gammapy_pulls_all_closed)
-issues_text = list_items(f'\n### issues opened last week (less than {days} days ago): \n', gammapy_issues.get_page(0))
+merged_text = list_items(f'\n### PRs merged last week (less than {days} days ago): \n', gammapy_pulls_all_closed, True)
+issues_text = list_created_issues(f'\n### issues opened last week (less than {days} days ago): \n', gammapy_issues)
 
 report = title + report_heading + pulls_text + merged_text + issues_text + report_footer
 
