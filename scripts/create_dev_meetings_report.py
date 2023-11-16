c2b62d2ac9dee57e90305a5e8bbba18cfacc113e
stahanna <140522054+stahanna@users.noreply.github.com>
2023-11-16 13:19:34 +0100

    Creation meetings reports (#152)
    * adding create meeting report
    
    * adding first yml
    
    * test workflow file
    
    * correct cron
    
    * correct cron again
    
    * basic test workflow
    
    * basic test cron
    
    * basic test cron 2
    
    * basic test cron 3
    
    * basic test cron 4
    
    * basic test cron 5 - Tuesday past 11
    
    * basic test cron 6 - Tuesday every minute
    
    * basic test cron 6 -with name -  Tuesday 11:20
    
    * test with secret token
    
    * draft create without token
    
    * draft create without token correction1
    
    * draft create without token correction2
    
    * draft create without token correction4
    
    * draft create without token correction5
    
    * draft create without token correction6
    
    * draft create without token correction7
    
    * adding requirements.txt for draft-create test
    
    * draft-create test with github repository
    
    * adding secret token as env -draft create test
    
    * adding secret token as env -draft create test2
    
    * test workflow without token - v min
    
    * testing echo of env vars
    
    * test get repo with token
    
    * test crete_dev_meetings_report
    
    * adding git commands to yml
    
    * adding email config to yml
    
    * adding correct email to yml
    
    * modif git config in yml
    
    * auto create meeting report
    
    * Update create_dev_meetings_report.py
    
    adding license, comment, cleaning a little
    
    * Update dev_meeting_reports.yml
    
    modifying comments
    
    * auto create meeting report
    
    * Update dev_meeting_reports.yml
    
    schedule on cron (Fridays at 6am)
    
    * Delete dev-meetings/2023/2023-11-05/README.md
    
    removing test report file
    
    * Delete dev-meetings/2023/2023-11-08/README.md
    
    removing test report file
    
    * Update dev_meeting_reports.yml
    
    changing cron for 4pm (for tests only)
    
    * Update requirements.txt
    
    delete double line
    
    * Delete draft_create.py
    
    delete draft create (again?)
    
    * Create .keep
    
    temp file for creating a directory
    
    * Rename create_dev_meetings_report.py to scripts/create_dev_meetings_report.py
    
    moving file
    
    * Update dev_meeting_reports.yml
    
    back to 6 am on Friday, update of the path for the run
    
    * Delete scripts/.keep
    
    * Update .github/workflows/dev_meeting_reports.yml
    
    ---------
    
    Co-authored-by: Régis Terrier <regis.terrier@m4x.org>

---
+++ scripts/create_dev_meetings_report.py
@@ -0,0 +1,67 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This script will run once a week (on Friday at 6 am? dev_meeting_reports.yml defines the time
+with cron syntax) to create a Gammapy Developer Meeting Report containing pull requests which have been
+opened or merged, as well as issues which have been opened since the previous meeting
+"""
+
+import os
+from datetime import datetime, timedelta
+import pytz
+from github import Github
+
+current_directory = '.'
+
+today = str(datetime.today().strftime('%Y-%m-%d'))
+time_now = str(datetime.today().strftime('%d/%m/%Y, %H:%M:%S'))
+today_long = str(datetime.today().strftime('%A, %B %d, %Y'))
+
+today_report = os.path.join(current_directory, "dev-meetings", str(datetime.today().date().year), today)
+isExist = os.path.exists(today_report)
+
+title = f'# Gammapy Developer Meeting \n'
+report_heading = f' * {today_long}, at 2 pm (CET) \n ' \
+                 '* Gammapy Developer Meeting on Zoom (direct link on Slack) \n' \
+                 '# Agenda\n'
+
+report_footer = f'\n report created at {time_now}'
+
+# ***************** connecting to the repository of gammapy with token ***************
+
+token = os.environ["SUPER_SECRET"]
+gh = Github(token)
+repo_gammapy = gh.get_repo("gammapy/gammapy")
+
+# ***************** getting pulls**********************
+gammapy_pulls_all_open = repo_gammapy.get_pulls('open')
+gammapy_pulls_all_closed = repo_gammapy.get_pulls('closed')
+gammapy_issues = repo_gammapy.get_issues()
+pulls_page1 = gammapy_pulls_all_open.get_page(0)
+
+# *************** one week long **********************
+days = 8
+d = datetime.today() - timedelta(days=days)
+
+
+def list_items(text, items):
+    for i in items:
+        if i.created_at.replace(tzinfo=pytz.utc) > d.replace(tzinfo=pytz.utc):
+            text = text + f'* [#{i.number}]({i.html_url}) {i.title} - {i.user.name}\n'
+    return text
+
+
+pulls_text = list_items(f'\n### PRs opened last week (less than {days} days ago): \n', gammapy_pulls_all_open)
+merged_text = list_items(f'\n### PRs merged last week (less than {days} days ago): \n', gammapy_pulls_all_closed)
+issues_text = list_items(f'\n### issues opened last week (less than {days} days ago): \n', gammapy_issues.get_page(0))
+
+report = title + report_heading + pulls_text + merged_text + issues_text + report_footer
+
+if not isExist:
+    os.makedirs(today_report)
+today_report_md = os.path.join(today_report, "README.md")
+
+with open(today_report_md, "w") as f:
+    f.write(report)
+
+if __name__ == "__main__":
+    print('all is good')
